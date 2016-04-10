5fa194a16e5be108d267761b920f91336f279a3a
XanderPSON <xwpeterson@gmail.com>
2016-04-09 19:11:43 -0700

    Add working modal login and signup function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,23 @@
 module ApplicationHelper
 	def header(text)
-	  content_for(:header) { text.to_s }
+		content_for(:header) { text.to_s }
+	end
+
+	def resource
+		@resource ||= User.new
+	end
+
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+ 	 # @devise_mapping ||= request.env["devise.mapping"]
+	end
+
+	def resource_name
+		# :user
+	  devise_mapping.name
+	end
+
+	def resource_class
+	  devise_mapping.to
 	end
 end
